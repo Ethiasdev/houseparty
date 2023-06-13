8eeba7091e637385d729325fbaf52c814f25d043
Daryii <112935597+Daryii@users.noreply.github.com>
2023-06-13 13:58:29 +0200

    Daryi push
    De Variables en Functions netjes gemaakt en de onclick naar addEventListener verandert

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
 dragElement(document.getElementById("mydiv"));
 dragElement(document.getElementById("test"));
 
+
+// dragElement function 
+
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
-    // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement() {
-    // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
   }
@@ -53,8 +49,7 @@ var dice = {
 }
 
 
-
-//Prints dice roll to the page
+// functions printNumber,daresbutton en truthbutton
 
 function printNumber(number) {
   var placeholder = document.getElementById('placeholder');
@@ -63,13 +58,30 @@ function printNumber(number) {
 
 var button = document.getElementById('button');
 
-button.onclick = function() {
+button.addEventListener("click", function(){
   var result = dice.roll();
   printNumber(result);
   console.log(result)
-};
+});
+
+var truthButton = document.getElementById("truth-btn");
+truthButton.addEventListener("click", function() {
+	var randomIndex = Math.floor(Math.random() * truthQuestions.length);
+	var question = truthQuestions[randomIndex];
+	alert(question);
+});
+
+var dareButton = document.getElementById("dare-btn");
+dareButton.addEventListener("click", function() {
+	var randomIndex = Math.floor(Math.random() * dareQuestions.length);
+	var question = dareQuestions[randomIndex];
+	alert(question);
+});
+
+
 
 // Truth questions
+
 var truthQuestions = [
 	"Wat is het ergste dat je ooit hebt gedaan?",
 	"Wat is je grootste angst?",
@@ -93,6 +105,7 @@ var truthQuestions = [
 ];
 
 // Dare questions
+
 var dareQuestions = [
 	"Twerk even",
 	"Doe ijsblokjes in je broek",
@@ -118,25 +131,4 @@ var dareQuestions = [
 
 
 ];
-
-// Get truth button element and add event listener
-var truthButton = document.getElementById("truth-btn");
-truthButton.addEventListener("click", function() {
-	// Get random truth question
-	var randomIndex = Math.floor(Math.random() * truthQuestions.length);
-	var question = truthQuestions[randomIndex];
-	// Show question in alert box
-	alert(question);
-});
-
-// Get dare button element and add event listener
-var dareButton = document.getElementById("dare-btn");
-dareButton.addEventListener("click", function() {
-	// Get random dare question
-	var randomIndex = Math.floor(Math.random() * dareQuestions.length);
-	var question = dareQuestions[randomIndex];
-	// Show question in alert box
-	alert(question);
-});
-
 
